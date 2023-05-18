f84aa154a64016e4854afbda2cb37536e27d64a5
Ayush <ayushrajpillai@gmail.com>
2023-05-19 03:03:01 +0530

    Initilased the project and created folder structure

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View  >
+<Text className='text-slate-900 font-bold'>Hello</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
